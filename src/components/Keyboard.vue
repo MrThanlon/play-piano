ac77e10f2726c1bdc0ecaed5a1c8171aeb410a28
huangziyi <huangziyi@canaan-creative.com>
2024-09-30 15:59:51 +0800

    add piano keyboard
    Signed-off-by: huangziyi <huangziyi@canaan-creative.com>

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,38 +1,76 @@
 <script setup lang="ts">
 import { defineProps, ref, onMounted } from 'vue'
 
-const canvas = ref<HTMLCanvasElement>()
-let context: CanvasRenderingContext2D
+const props = defineProps([])
 
-const props = defineProps(['foo'])
-
-console.log(props.foo)
-
-function drawGroup(ctx: CanvasRenderingContext2D, x: number, y: number) {
-  ctx.moveTo(x, y)
-  ctx.lineTo(x, y + 30)
-  ctx.lineTo(x + 3, y + 30)
-  ctx.stroke()
+const w = 1
+type Key = { color: 'white' | 'black', points: number[]}
+const k: Key[] = genKeys(2)
+for (let i = 0; i < 7; i++) {
+  k.push(...genKeys(3, i * w * 12 + w * 3))
+  k.push(...genKeys(4, i * w * 12 + w * 8))
 }
+k.push(...genKeys(1, w * 87))
+const keys = ref<Key[]>(k)
 
 onMounted(() => {
-  if (canvas.value) {
-    const ctx = canvas.value.getContext('2d')
-    if (ctx) {
-      context = ctx
-    }
-    context.fillStyle = '#ffffffff'
-    context.fillRect(0, 0, canvas.value.width, canvas.value.height)
-    drawGroup(context, 0, 0)
-  }
+  console.log(props)
 })
+
+function genKeys(white: number, offset: number = 0, w: number = 1, h: number = 10, bh: number = 6): Key[] {
+  const total = white * 2 - 1
+  return Array(total).fill(0).map((_, idx) => {
+    const points: number[] = []
+    if (idx & 1) {
+      // black
+      points.push(
+        offset + w * idx, 0,
+        offset + w * idx, bh,
+        offset + w * idx + w, bh,
+        offset + w * idx + w, 0,
+      )
+    } else {
+      // white
+      const bi = idx / 2
+      const left = w * total / white * bi
+      const right = w * total / white * (bi + 1)
+      points.push(
+        offset + w * idx, 0,
+        offset + w * idx, bh,
+        offset + left, bh,
+        offset + left, h,
+        offset + right, h,
+        offset + right, bh,
+        offset + w * idx + w, bh,
+        offset + w * idx + w, 0
+      )
+    }
+    return {
+      color: (idx & 1) ? 'black' : 'white',
+      points
+    }
+  })
+}
 
 </script>
 
 <template>
-  <canvas ref="canvas" style="width: 100%; height: 100px;"></canvas>
+  <svg viewBox="0 0 88 10">
+    <polygon v-for="k in keys" :class="k.color" :points="k.points.toString()"></polygon>
+  </svg>
 </template>
 
 <style scoped>
-
+.white {
+  fill: white;
+  stroke: black;
+  stroke-width: 0.2%;
+}
+.black {
+  fill: black;
+}
+svg {
+  width: 100%;
+  background-color: white;
+}
 </style>
